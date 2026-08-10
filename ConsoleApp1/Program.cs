83f79c5b698a928ae16c7099f4a0657ebee62827
VoVanHai <vovanhai@ueh.edu.vn>
2026-08-10 16:25:33 +0700

    cập nhật lớp cũ, thêm lớp mới

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Xin chào pà kon");
         Console.WriteLine("Hello, World!");
+
+        Console.WriteLine($"2 + 2 = {2+2}");
     }
 }
---
+++ ConsoleApp1/test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class test
+    {
+    }
+}
